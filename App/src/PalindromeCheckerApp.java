283c2ae9bc4d1d9f851bb35d3602e5cc97ae4b3a
AkshitGaurana <akshit.gaurana@gmail.com>
2026-03-02 16:07:40 +0530

    UC12-StrategyPatterForPalindromeAlgorithm Pushed

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,23 +1,30 @@
-class PalindromeService {
-    public boolean isPalindrome(String input) {
-        input = input.toLowerCase();
-        int left = 0;
-        int right = input.length() - 1;
-        while (left < right) {
-            if (input.charAt(left) != input.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
+import java.util.Scanner;
+
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+
+class ReverseStrategy implements PalindromeStrategy {
+    public boolean check(String input) {
+        String reversed = new StringBuilder(input).reverse().toString();
+        return input.equals(reversed);
     }
 }
 
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
-        PalindromeService service = new PalindromeService();
-        String input = "noon";
-        System.out.println(service.isPalindrome(input));
+
+        Scanner sc = new Scanner(System.in);
+        PalindromeStrategy strategy = new ReverseStrategy();
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        boolean result = strategy.check(input);
+
+        System.out.println(result ? "Palindrome" : "Not a Palindrome");
+
+        sc.close();
     }
 }
