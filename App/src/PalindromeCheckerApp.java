1d412b6dbc4a538f9d3addcfe2029864b72fe5b8
AkshitGaurana <akshit.gaurana@gmail.com>
2026-03-02 16:02:59 +0530

    import java.util.Scanner; Pushed

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,20 +1,19 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
-    public static boolean isPalindrome(String input) {
-        input = input.replaceAll("\\s+", "").toLowerCase();
-        int left = 0;
-        int right = input.length() - 1;
-        while (left < right) {
-            if (input.charAt(left) != input.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
-    }
 
     public static void main(String[] args) {
-        String input = "A man a plan a canal Panama";
-        System.out.println(isPalindrome(input));
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        input = input.replaceAll("\\s+", "").toLowerCase();
+
+        String reversed = new StringBuilder(input).reverse().toString();
+
+        System.out.println(input.equals(reversed) ? "Palindrome" : "Not a Palindrome");
+
+        sc.close();
     }
 }
